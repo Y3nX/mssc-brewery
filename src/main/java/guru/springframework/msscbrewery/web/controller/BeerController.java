7a031c11724e6923d049b82334c7beb821877a62
Mykyta Yamshynskyi <mykyta_yamshynskyi@epam.com>
2021-02-16 00:38:08 +0200

    [Mssc-Brewery] Create Customer Post Endpoint with Spring MVC

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package guru.springframework.msscbrewery.web.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,14 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping // POST - create new beer
+    public ResponseEntity handlePost(BeerDto beerDto) {
+        final BeerDto createdBeer = beerService.saveNewBeer(beerDto);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        // todo add hostname to url
+        httpHeaders.add("Location", "/api/v1/beer/" + createdBeer.getId().toString());
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
     }
 }
