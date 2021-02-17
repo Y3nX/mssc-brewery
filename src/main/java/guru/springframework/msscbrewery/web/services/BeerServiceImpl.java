0098cee3b612cbce174907dc9792697a60228342
Mykyta Yamshynskyi <mykyta_yamshynskyi@epam.com>
2021-02-18 00:49:44 +0200

    [Mssc-Brewery] Add @RequestBody

--- src/main/java/guru/springframework/msscbrewery/web/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/web/services/BeerServiceImpl.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbrewery.web.services;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
 
@@ -22,5 +23,10 @@ public class BeerServiceImpl implements BeerService {
         return BeerDto.builder()
                 .id(UUID.randomUUID())
                 .build();
+    }
+
+    @Override
+    public void updateBeer(final UUID beerId, final @RequestBody BeerDto beerDto) {
+        // todo impl - would add a real impl to update beer
     }
 }
